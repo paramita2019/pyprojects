11c98b903a3c0cb1f26545a8ea34ae607fe3b42c
Paramita Pradhan <149745946+papradhan@users.noreply.github.com>
2023-12-23 19:56:30 -0500

    explore chat and text responses

--- play_langchain.py
+++ play_langchain.py
@@ -1,19 +1,33 @@
 # Import prerequisite libraries
-import os
 import openai
-import langchain
-from langchain.llms import openai
 from langchain.llms import OpenAI
-
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import BaseOutputParser
+#from langchain.prompts import ChatPromptTemplate
+#from langchain.schema import BaseOutputParser
 
 from secret_key import openai_key
 openai.api_key = openai_key
+llm = OpenAI(openai_api_key=(openai.api_key))   # integrate langchain with openai
 
-llm = OpenAI(openai_api_key=(openai.api_key))
-
+text = """
 response = llm.invoke("List all the chinese zodiac animals")
 print ("Lets play Langchain")
 print (response)
+"""
+
+# chatbot
+chat = ChatOpenAI(openai_api_key=(openai.api_key))     # frequently used - HumanMessage, AIMessage, and SystemMessage
+from langchain.schema.messages import HumanMessage, SystemMessage, AIMessage
+messages = [
+    SystemMessage(content="You are Deutsche Bank"),
+    HumanMessage(content="What line of business are you associated with"), # AS A HUMAN 
+    ]
+response = chat.invoke(messages)
+print(response)
+
+messages = [
+    SystemMessage(content="You are Deutsche Bank"),
+    AIMessage(content="What can you tell me about yourself") # AN AN AI LM
+    ]
+response = chat.invoke(messages)
+print(response)
